5c5176fc102023ac5f5682bb263049664d3b1759
khoir-roni <khoir.roni@gmail.com>
2021-08-09 15:04:40 +0700

    commit parseCode

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/model/restaurant_model.dart';
-import 'package:flutter/services.dart' ;
+import 'package:flutter/services.dart' as rootBundle;
 import 'dart:convert';
 import 'dart:async';
 
@@ -10,36 +10,88 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Future<String> loadRestaurantFromAssets() async {
-    return await rootBundle.loadString('local_restaurant.json');
-  }
-
-  List drinks = [];
-  Future loadPerson() async {
-    String jsonString = await loadRestaurantFromAssets();
-    final jsonResponse = json.decode(jsonString);
-    // print('jsonResponse: ${jsonResponse}');
-    //print('restaurants: ${jsonResponse['restaurants']['id']}');
-    ListRestaurant listrestaurants = ListRestaurant.fromJson(jsonResponse);
-    // print('Name: ${listrestaurants.restaurants[1].menus.drinks[1].name}');
-    //print(listrestaurants.restaurants.toString());
-
-    setState(() {
-      drinks = listrestaurants.restaurants[1].menus.drinks;
-    });
-    print(drinks);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    loadPerson();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      body: FutureBuilder(
+        future: ReadJsonData(),
+        builder: (context, data) {
+          if (data.hasError) {
+            //in case if error found
+            return Center(child: Text("${data.error}"));
+          } else if (data.hasData) {
+            //once data is ready this else block will execute
+            // items will hold all the data of DataModel
+            //items[index].name can be used to fetch name of product as done below
+            var items = data.data as List<ListRestaurant>;
+               return ListView.builder(
+                itemCount: items == null ? 0 : items.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            child: Image(
+                              image: NetworkImage(items[1].restaurants[index].pictureId.toString()),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          Expanded(
+                              child: Container(
+                            padding: EdgeInsets.only(bottom: 8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8, right: 8),
+                                  child: Text(
+                                    items[1].restaurants[index].name.toString(),
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8, right: 8),
+                                  child: Text(items[1].restaurants[index].city.toString()),
+                                ),
+                              ],
+                            ),
+                          ))
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          } else {
+            // show circular progress while data is getting fetched from json file
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
+  }
+
+  Future<List<ListRestaurant>> ReadJsonData() async {
+    //read json file
+    final jsondata =
+        await rootBundle.rootBundle.loadString('local_restaurant.json');
+    //decode json data as list
+    final list = json.decode(jsondata) as List<dynamic>;
+
+    //map json and initialize using DataModel
+    return list.map((e) => ListRestaurant.fromJson(e)).toList();
   }
 }
